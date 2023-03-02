04e07475b60f3598a171be27765866b8b98f541d
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-01 22:41:16 -0300

    :recycle: refactor: separando rotas

--- src/server.js
+++ src/server.js
@@ -3,43 +3,22 @@
 
 // Stateful vs Statless
 
-const database = new Database
 
 import http from 'node:http'
-import { randomUUID } from 'node:crypto'
-import { Database } from './database.js'
 import { json } from './middlewares/json.js'
+import { routes } from './routes.js';
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res);
 
-  if (method === 'GET' && url === '/users') {
-    const users = database.select('users')
+  const route = routes.find(route => {
+    return route.method === method && route.path === url
+  })
 
-    return res
-      .end(JSON.stringify(users))
-    // responsta só pode ser: string, buffer ou uint8array
-    // JSON - transicao de dados entra front e back (Javascript Object Notation)
-    // Muito utilizado na transicao de dados de front-back e back-back
-    // Se assemelha com muito a como as estruturas de dados são dentro do js
-    // representar arrays, objetos, tipos primitivos dentro de uma string
-    // transitar dados entre várias aplicações
-  }
-
-  if (method === 'POST' && url === '/users') {
-    const { name, email } = req.body
-
-    const user = {
-      id: randomUUID(),
-      name,
-      email
-    }
-
-    database.insert('users', user)
-
-    return res.writeHead(201).end()
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end('Not found')
@@ -47,3 +26,11 @@ const server = http.createServer(async (req, res) => {
 
 // localhost:3333
 server.listen(3333)
+
+
+    // responsta só pode ser: string, buffer ou uint8array
+    // JSON - transicao de dados entra front e back (Javascript Object Notation)
+    // Muito utilizado na transicao de dados de front-back e back-back
+    // Se assemelha com muito a como as estruturas de dados são dentro do js
+    // representar arrays, objetos, tipos primitivos dentro de uma string
+    // transitar dados entre várias aplicações
