3104ec8d6c9e776f4627cb6da5cb6c5c1845062a
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-02 11:42:34 -0300

    :construction: feat: rotas com parametros (regex)

---
+++ src/utils/build-route-path.js
@@ -0,0 +1,9 @@
+// '/users/:id'
+export function buildRoutePath(path) {
+  const routeParametersRegex = /:([a-zA-z]+)/g // retorno do indice 1
+  const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)') // $1 pega retorno na posicao 1 e coloca como nome (id)
+
+  const pathRegex = new RegExp(`^${pathWithParams}`)
+
+  return pathRegex
+}  
