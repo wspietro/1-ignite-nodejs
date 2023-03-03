6cba989f8a2c4b528637957cabf45b3d27ddc33c
Pietro Weg <pietro.sera@compasso.com.br>
2023-03-02 23:24:23 -0300

    :construction: feat: capturando query params

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -3,7 +3,7 @@ export function buildRoutePath(path) {
   const routeParametersRegex = /:([a-zA-z]+)/g // retorno do indice 1
   const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)') // $1 pega retorno na posicao 1 e coloca como nome (id)
 
-  const pathRegex = new RegExp(`^${pathWithParams}`)
+  const pathRegex = new RegExp(`^${pathWithParams}(?<query>\\?(.*))?$`)
 
   return pathRegex
 }  
